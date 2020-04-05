0f9432ad0ed87b4ff1d1cc877556480a981b0efd
Wailweb <wail.sdni@gmail.com>
2020-04-05 17:27:09 +0200

    push first commit in php

---
+++ entete.php
@@ -0,0 +1,5 @@
+<header>
+
+    <h2>coucou mon deuxieme commit php sur github </h2>
+
+</header>
---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>Site PHP</title>
+</head>
+<body>
+
+<h1>PHP
+            <?php echo "coucou c'est moi"; ?>
+</h1>
+    <?php include("entete.php"); ?>
+
+    <?php include("menus.php"); ?>
+
+
+
+
+</body>
+</html>
